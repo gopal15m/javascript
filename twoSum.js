d46455c9c0588658489e476c740dcbb36c0d4e55
gopal15m <gopal15m@gmail.com>
2022-02-08 16:05:23 -0500

    Create twoSum.js
    twoSum

---
+++ twoSum.js
@@ -0,0 +1,18 @@
+
+/**
+*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+*Input: nums = [2,7,11,15], target = 9
+*Output: [0,1]
+*Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+*/
+
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++){
+        for(let j = i+1; j < nums.length; j++){
+            if (nums[i]+ nums[j] == target){
+                return [i, j];
+            }
+        }
+    }
+    
+};
